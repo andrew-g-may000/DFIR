5a443f8c947fea7e112119284ee22708158eca8d
andrew-g-may000 <147677469+andrew-g-may000@users.noreply.github.com>
2023-12-05 11:15:01 -0600

    Update DFIRunitTests.py

--- DFIRunitTests.py
+++ DFIRunitTests.py
@@ -29,55 +29,75 @@ import unittest
 class StackTests(unittest.TestCase):
     # Testing the Stack class' push method
     def test_push(self):
+        # Arrange
         stack = Stack()
+        # Act
         stack.push(1)
         stack.push(2)
         stack.push(3)
+        # Assert
         self.assertEqual(stack.size(), 3)
 
     # Testing the Stack class' pop method
     def test_pop(self):
+        # Arrange
         stack = Stack()
         stack.push(1)
         stack.push(2)
         stack.push(3)
+        # Act
         item = stack.pop()
+        # Assert
         self.assertEqual(item, 3)
         self.assertEqual(stack.size(), 2)
 
     # Testing the Stack class' is_empty method
     def test_is_empty(self):
+        # Arrange
         stack = Stack()
+        # Assert
         self.assertTrue(stack.is_empty())
+        # Act
         stack.push(1)
+        # Assert
         self.assertFalse(stack.is_empty())
 
     # Testing the Stack class' size method
     def test_size(self):
+        # Arrange
         stack = Stack()
+        # Assert
         self.assertEqual(stack.size(), 0)
+        # Act
         stack.push(1)
         stack.push(2)
         stack.push(3)
+        # Assert
         self.assertEqual(stack.size(), 3)
 
     # Testing the Stack class' top method
     def test_top(self):
+        # Arrange
         stack = Stack()
         stack.push(1)
         stack.push(2)
         stack.push(3)
+        # Act
         top = stack.top()
+        # Assert
         self.assertEqual(top, 3)
         self.assertEqual(stack.size(), 3)
 
     # Testing the Stack class' print_stack_up method
     def test_print_stack_up(self):
+        # Arrange
         stack = Stack()
         stack.push("a")
         stack.push("b")
         stack.push("c")
+        # Act
         stack_string = stack.print_stack_up()
+        # Assert
         self.assertEqual(stack_string, "a\nb\nc\n")
 
 
@@ -86,15 +106,21 @@ class HashTableTests(unittest.TestCase):
 
     # Testing the HashTable class' set_value method
     def test_set_value(self):
+        # Arrange
         hash_table = HashTable()
+        # Act
         hash_table.set_value(3, "apple")
+        # Assert
         self.assertEqual(hash_table.get_value(3), "apple")
         
     # Testing the HashTable class' get_value method
     def test_get_value(self):
+        # Arrange
         hash_table = HashTable()
+        # Act
         hash_table.set_value(3, "apple")
         hash_table.set_value(6, "banana")
+        # Assert
         self.assertEqual(hash_table.get_value(3), "apple")
         self.assertEqual(hash_table.get_value(6), "banana")
         self.assertEqual(hash_table.get_value(13), "No record found")
@@ -102,19 +128,25 @@ class HashTableTests(unittest.TestCase):
 
     # Testing the HashTable class' print_hash_table method
     def test_print_hash_table(self):
+        # Arrange
         hash_table = HashTable()
+        # Act
         hash_table.set_value(3, "apple")
         hash_table.set_value(9, "cherry")
         hash_table.set_value(6, "banana")
+        # Assert
         self.assertEqual(hash_table.print_hash_table(), "3: apple\n9: cherry\n6: banana")
 
     # Testing the HashTable class' insertion_sort method
     def test_insertion_sort(self):
+        # Arrange
         hash_table = HashTable()
+        # Act
         hash_table.set_value(3, "apple")
         hash_table.set_value(9, "cherry")
         hash_table.set_value(6, "banana")
         hash_table.insertion_sort()
+        # Assert
         self.assertEqual(hash_table.print_hash_table(), "3: apple\n6: banana\n9: cherry")
 
 
